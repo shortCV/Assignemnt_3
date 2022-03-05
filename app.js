e11f92ae61afeb612ff7572809962c8f494c79a4
shortCV <cversace18@gmail.com>
2022-03-05 13:39:31 -0500

    circle added

--- app.js
+++ app.js
@@ -4,24 +4,25 @@ const http      = require('http');
 const server    = require('http').createServer(app);  
 const io        = require('socket.io')(server);
 
-const LISTEN_PORT   = 8080;
+const LISTEN_PORT   = 8081;
 
 server.listen(LISTEN_PORT);
 app.use(express.static(__dirname + '/public')); //set root path of server ...
 console.log("Listening on port: " + LISTEN_PORT );
+
 
 //our routes
 app.get( '/', function( req, res ){ 
     res.sendFile( __dirname + '/public/index.html' );
 });
 
-app.get( '/2D', function( req, res ){ 
+app.get( '/join', function( req, res ){ 
     res.sendFile( __dirname + '/public/2D.html' );
 });
 
-app.get( '/3D', function( req, res ){ 
+/*app.get( '/3D', function( req, res ){ 
     res.sendFile( __dirname + '/public/3D.html' );
-});
+});*/
 
 //socket.io / websockets stuff
 
@@ -32,13 +33,9 @@ io.on('connection', (socket) => {
         console.log(socket.id + ' disconnected');
     });
 
-    socket.on('red', (data) => {
-        console.log('red event received');
-        io.sockets.emit('color_change', {r:255, g:0, b:0});
-    });
+    /*socket.on('enemy', (data) => {
+        console.log('enemy event received');
+        //io.sockets.emit('point_collect');
+    });*/
 
-    socket.on('blue', (data) => {
-        console.log('blue event received');
-        io.sockets.emit('color_change', {r:0, g:0, b:255});
-    });
 });
